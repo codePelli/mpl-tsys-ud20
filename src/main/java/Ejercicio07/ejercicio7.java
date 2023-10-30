f0883fcc4aa7dc22ce7ebbda80d9d373febb0b0f
Marc Pellicer <marcpelli3@gmail.com>
2023-10-30 15:10:41 +0100

    Ejercicio7 DONE

--- src/main/java/Ejercicio07/ejercicio7.java
+++ src/main/java/Ejercicio07/ejercicio7.java
@@ -18,13 +18,74 @@ public class ejercicio7 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField tfCantidad;
+	private JTextField tfResultado;
+	private boolean conversion;
 
 
 	public ejercicio7() {
+		getContentPane().setLayout(null);
 		
+		JLabel lblNewLabel = new JLabel("Cantidad a convertir");
+		lblNewLabel.setBounds(10, 11, 105, 14);
+		getContentPane().add(lblNewLabel);
+		
+		tfCantidad = new JTextField();
+		tfCantidad.setBounds(125, 8, 86, 20);
+		getContentPane().add(tfCantidad);
+		tfCantidad.setColumns(10);
+		
+		JLabel lblResultado = new JLabel("Resultado");
+		lblResultado.setBounds(10, 57, 105, 14);
+		getContentPane().add(lblResultado);
+		
+		tfResultado = new JTextField();
+		tfResultado.setColumns(10);
+		tfResultado.setBounds(125, 54, 86, 20);
+		getContentPane().add(tfResultado);
+		
+		JButton btnConversion = new JButton("€ a ptas");
+		btnConversion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (conversion) {
+					
+					double euros = Double.parseDouble(tfCantidad.getText());
+					double pesetas = euros * 166.386;
+					tfResultado.setText(String.valueOf(pesetas));
+				} else {
+					
+					double pesetas = Double.parseDouble(tfCantidad.getText());
+					double euros = pesetas / 166.3866;
+					tfResultado.setText(String.valueOf(euros));
+				}
 
+					
+			}
+		});
+		btnConversion.setBounds(125, 150, 89, 23);
+		getContentPane().add(btnConversion);
+		
+		JButton btnCambiar = new JButton("Cambiar");
+		btnCambiar.setBounds(224, 150, 89, 23);
+		getContentPane().add(btnCambiar);
+		btnCambiar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				conversion = !conversion;
+				
+				if(conversion) {
+					
+					btnConversion.setText("€ a ptas");
+				} else {
+					
+					btnConversion.setText("ptas a €");
+				}
+					
+			}
+		});
+		
 		
 		
 	}
-
 }
